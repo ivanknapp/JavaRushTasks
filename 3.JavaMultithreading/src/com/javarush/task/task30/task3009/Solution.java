754054442cef0ee40d77ab9fb5ddf38d8dfce8f6
van2blockhead <ivanknapp@yandex.ru>
2017-12-18 22:35:12 +0400

    some adds

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task30.task3009;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Палиндром?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getRadix("112"));        //expected output: [3, 27, 13, 15]
+        System.out.println(getRadix("123"));        //expected output: [6]
+        System.out.println(getRadix("5321"));       //expected output: []
+        System.out.println(getRadix("1A"));         //expected output: []
+    }
+
+    private static Set<Integer> getRadix(String number){
+        Set<Integer> set = new HashSet<>();
+
+        try {
+            BigInteger bigInteger = new BigInteger(number, 10);
+            for (int i = 2; i <= 36; i++) {
+                String sb = bigInteger.toString(i);
+/*                if (sb.length()==1){
+                    set.add(Integer.parseInt(sb.toString()));
+                    break;
+                }*/
+                if (sb.equals(new StringBuilder(sb).reverse().toString())) {
+                    set.add(i);
+                }
+            }
+
+        } catch (NumberFormatException e) {
+
+        }
+
+
+        return set;
+    }
+}
